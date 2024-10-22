99e5e71a33cd295266d048f168a16f74bccfcf29
Jakub Černík <jakubcernik15@gmail.com>
2024-10-22 13:19:57 +0200

    + added ShaderLoader class

--- cviko5/cviko5/ShaderProgram.cpp
+++ cviko5/cviko5/ShaderProgram.cpp
@@ -1,58 +1,18 @@
-//ShaderProgram.cpp
-
 #include "ShaderProgram.h"
-#include <GL/glew.h>
+#include "ShaderLoader.h"  // Pøidání nového souboru ShaderLoader
 #include <glm/gtc/type_ptr.hpp>
-#include <iostream>
-using namespace std;
 
 ShaderProgram::ShaderProgram(const string& vertexShaderPath, const string& fragmentShaderPath) {
-    programID = glCreateProgram();
-
-    // Create shader objects
-    VertexShader vertexShader(vertexShaderPath);
-    FragmentShader fragmentShader(fragmentShaderPath);
-
-    // Compile shaders
-    vertexShader.compileShader();
-    fragmentShader.compileShader();
-
-    // Attach and link
-    attachVertexShader(vertexShader);
-    attachFragmentShader(fragmentShader);
-    linkProgram();
+    ShaderLoader shaderLoader;
+    programID = shaderLoader.loadShader(vertexShaderPath.c_str(), fragmentShaderPath.c_str());
 }
 
 ShaderProgram::~ShaderProgram() {
     glDeleteProgram(programID);
 }
 
-void ShaderProgram::attachVertexShader(const VertexShader& vertexShader) {
-    glAttachShader(programID, vertexShader.getID());
-}
-
-void ShaderProgram::attachFragmentShader(const FragmentShader& fragmentShader) {
-    glAttachShader(programID, fragmentShader.getID());
-}
-
-void ShaderProgram::linkProgram() {
-    glLinkProgram(programID);
-
-    GLint success;
-    glGetProgramiv(programID, GL_LINK_STATUS, &success);
-    if (!success) {
-        char infoLog[512];
-        glGetProgramInfoLog(programID, 512, nullptr, infoLog);
-        printf("Shader program linking failed\n%s", infoLog);
-    }
-}
-
 void ShaderProgram::use() {
-    glUseProgram(programID); // Setting the program as active
-}
-
-unsigned int ShaderProgram::getProgramID() const {
-    return programID;
+    glUseProgram(programID); // Nastavení programu jako aktivního
 }
 
 void ShaderProgram::setUniform(const string& name, const glm::mat4& matrix) {
--- cviko5/cviko5/ShaderProgram.h
+++ cviko5/cviko5/ShaderProgram.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "VertexShader.h"
-#include "FragmentShader.h"
 #include "Observer.h"
 #include <glm/glm.hpp>
 #include <string>
@@ -13,12 +11,8 @@ public:
     ShaderProgram(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
     ~ShaderProgram();
 
-    void attachVertexShader(const VertexShader& vertexShader); // For attaching vertex shader
-    void attachFragmentShader(const FragmentShader& fragmentShader); // For attaching fragment shader
     void setUniform(const std::string& name, const glm::mat4& matrix);
-    void linkProgram();
     void use();
-    unsigned int getProgramID() const;
 
     void onNotify(const glm::mat4& viewMatrix, const glm::mat4& projectionMatrix) override;
 
