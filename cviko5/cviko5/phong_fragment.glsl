0f873e1726aefcada97e1ddebaf3a5eaf7b87e7c
Jakub Černík <jakubcernik15@gmail.com>
2024-11-06 18:06:20 +0100

    brought back the higher exponent

--- cviko5/cviko5/phong_fragment.glsl
+++ cviko5/cviko5/phong_fragment.glsl
@@ -19,7 +19,7 @@ void main() {
     // Specular lighting
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 1);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = lightColor * spec;
 
     //specular pouze pro kladne hodnoty
