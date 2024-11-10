c2f3720f9765b96c12641c12c34223aad6a53aeb
Jakub Černík <jakubcernik15@gmail.com>
2024-11-10 17:25:15 +0100

    + IF statement

--- cviko5/cviko5/phong_fragment.glsl
+++ cviko5/cviko5/phong_fragment.glsl
@@ -19,10 +19,15 @@ void main() {
     // Specular lighting
     vec3 viewDir = normalize(viewPos - fragWorldPosition);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    float spec = max(dot(viewDir, reflectDir), 0.0);
+    
+    if (spec >= 0.0) {
+        spec = pow(spec, 32);
+    } else {
+        spec = 0.0;
+    }
+    
     vec3 specular = lightColor * spec;
-
-    //specular pouze pro kladne hodnoty
 
     FragColor = vec4(diffuse + specular, 1.0);
 }
